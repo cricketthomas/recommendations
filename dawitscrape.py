0ef6bf52d2b3412cb32aae9590a9b99379b5acef
Arthur Thomas IV <acthomasiv@gmail.com>
2018-11-14 16:42:37 -0500

    updates to the scripts, began researching how to do this..

--- dawitscrape.py
+++ dawitscrape.py
@@ -4,26 +4,28 @@ import json
 url = urllib.request.urlopen('https://edraft.com/nfl/stats/leaders/').read() #so this link searches for all content on this url. 
 soup = bs.BeautifulSoup(url,'lxml')
 stats = [] #storing data here. 
+
 #rows =  soup.find("t", { "class" : "mod-content" }).find('tr'),
 
 for data in soup.select(".Container"):
     a = str(data.text)
     stat = data.find("td", attrs={"class": "TextLeft Name"})
     stat1 = data.find_all("TextLeft Name")
-    myList = []
+    all_stats ={}
     count = 0
     for link in data.find_all("td", attrs={"class": "TextLeft Name"}):
         player = "player"
         player = '{}{}'.format(player, count)
         count += 1
-        playerData = player +": " + link.text +" Count: " + link.next_sibling.get_text()
-        myList.append(playerData)
-    #print(myList)
+        playerData = player +":" + link.text +" Count: " + link.next_sibling.get_text()
+        #myList.append(playerData)
+        all_stats[player] =  link.text +" Count: " + link.next_sibling.get_text()    
+    #print(myDict)
     #stats.append(a) 
     record = {
         "title": data.td.get_text(),
         "top_player": stat.get_text() +' ' + stat.next_sibling.get_text(),
-        "all_stats ": myList
+        "all_stats": myDict
     }
     stats.append(record)
 #print(*stats, sep='\n')
@@ -34,4 +36,3 @@ print(json.dumps({"nfldata": stats}, indent=3))
 #making the file so you can read it in your script. 
 with open("nflstats.json", "w") as writeJSON:
     json.dump(json.dumps({"nfldata": stats}, indent=3), writeJSON, ensure_ascii=False)
-
